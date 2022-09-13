ec78c2d48b5c8711039b967aa16126a9f807c873
jasmine-alvarez <jasmine.rcalvarez@gmail.com>
2022-09-13 13:20:16 -0400

    Save smartpca cmd to script

--- code/run_smartpca.sh
+++ code/run_smartpca.sh
@@ -3,4 +3,4 @@
 
 plink --file data/hapmap1 --make-bed --out data/hapmap1
 
-#%s/^\s*//g
+../EIG/bin/smartpca.perl -i data/hapmap1.bed -a data/hapmap1.pedsnp -b data/hapmap1.pedind -o data/hapmap1.pca -p data/hapmap1.plot -e data/hapmap1.eval -l data/hapmap1.log
