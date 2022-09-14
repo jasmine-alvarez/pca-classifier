b552c816736c3e7fafd8ef044dcc388e628e3b7e
jasmine-alvarez <jasmine.rcalvarez@gmail.com>
2022-09-14 00:31:34 -0400

    Read evac and eval data in

--- code/run_smartpca.sh
+++ code/run_smartpca.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# TODO LD-pruned to r2 < 0.1, high callrate, biallelic, AF > 0.01
+plink --file data/acs_mini_project --make-bed --out data/acs_mini_project 
 
-plink --file data/hapmap1 --make-bed --out data/hapmap1
+bcftools view -m2 -M2 -v snps data/acs_mini_project.vcf.gz | \
+bcftools filter -e 'AF < 0.1 & F_MISSING > 0.05' | \
+bcftools norm -f data/Homo_sapiens.GRCh37.dna.primary_assembly.fa | \
+bgzip -c > data/acs_mini_project.filtered.vcf.gz
 
-../EIG/bin/smartpca.perl -i data/hapmap1.bed -a data/hapmap1.pedsnp -b data/hapmap1.pedind -o data/hapmap1.pca -p data/hapmap1.plot -e data/hapmap1.eval -l data/hapmap1.log
+../EIG/bin/smartpca.perl -i data/acs_mini_project.bed -a data/acs_mini_project.pedsnp -b data/acs_mini_project.pedind -k 60 -t 60 -o data/acs_mini_project.pca -p data/acs_mini_project.plot -e data/acs_mini_project.eval -l data/acs_mini_project.log
+
+printf %.10f\n 1 
